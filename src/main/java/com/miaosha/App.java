80db2206bdadbdab89067f8bb5c64985552e005e
shawbs <xxw.show@qq.com>
2019-01-16 11:22:20 +0800

    Invalid bound statement (not found): com.miaosha.dao.UserDOMapper.selectByPrimaryKey

--- src/main/java/com/miaosha/App.java
+++ src/main/java/com/miaosha/App.java
@@ -1,7 +1,12 @@
 package com.miaosha;
 
+import com.miaosha.dao.UserDOMapper;
+import com.miaosha.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.miaosha"})
 @RestController
+@MapperScan("com.miaosha.dao")
 public class App
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String Home(){
-        return "hello wolrd";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "用户不存在";
+        }else{
+            return userDO.getName();
+        }
     }
 
     public static void main( String[] args )
